8c6abe30bdc5a5bd43d9b4425cf5c4e0359a9775
Parthesh28 <partheshpurohit28@gmail.com>
2025-10-21 21:32:37 +0530

    completed upto quiz2

---
+++ rustlings/exercises/quizzes/quiz2.rs
@@ -0,0 +1,97 @@
+// This is a quiz for the following sections:
+// - Strings
+// - Vecs
+// - Move semantics
+// - Modules
+// - Enums
+//
+// Let's build a little machine in the form of a function. As input, we're going
+// to give a list of strings and commands. These commands determine what action
+// is going to be applied to the string. It can either be:
+// - Uppercase the string
+// - Trim the string
+// - Append "bar" to the string a specified amount of times
+//
+// The exact form of this will be:
+// - The input is going to be a Vector of 2-length tuples,
+//   the first element is the string, the second one is the command.
+// - The output element is going to be a vector of strings.
+
+enum Command {
+    Uppercase,
+    Trim,
+    Append(usize),
+}
+
+mod my_module {
+    use super::Command;
+
+    // TODO: Complete the function as described above.
+    // pub fn transformer(input: ???) -> ??? { ??? }
+
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        // Create a new, empty vector to store our results.
+        let mut output = Vec::new();
+
+        // Iterate over the input vector. This will move the (String, Command)
+        // out of the input vector on each iteration.
+        for (string, command) in input {
+            // Match on the command to decide what to do
+            match command {
+                // If Command is Uppercase, convert the string and push it
+                Command::Uppercase => {
+                    output.push(string.to_uppercase());
+                }
+                // If Command is Trim, trim the string and push it
+                // .trim() returns an &str, so we need .to_string()
+                Command::Trim => {
+                    output.push(string.trim().to_string());
+                }
+                // If Command is Append(n), repeat "bar" n times,
+                // concatenate it with the string, and push it.
+                Command::Append(n) => {
+                    // "bar".repeat(n) creates a new String
+                    // We can add a String and a &str
+                    output.push(string + &"bar".repeat(n));
+                }
+            }
+        }
+
+        // Return the new vector
+        output
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::my_module::transformer;
+
+    // TODO: What do we need to import to have `transformer` in scope?
+    // use ???;
+    use super::Command;
+
+    #[test]
+    fn it_works() {
+        let input = vec![
+            ("hello".to_string(), Command::Uppercase),
+            (" all roads lead to rome! ".to_string(), Command::Trim),
+            ("foo".to_string(), Command::Append(1)),
+            ("bar".to_string(), Command::Append(5)),
+        ];
+        let output = transformer(input);
+
+        assert_eq!(
+            output,
+            [
+                "HELLO",
+                "all roads lead to rome!",
+                "foobar",
+                "barbarbarbarbarbar",
+            ]
+        );
+    }
+}
